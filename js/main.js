fd8d9383f7b6ea893a90619bb48d59b11f2b3cda
muhammad.02 <m7319906>
2024-05-29 20:57:03 +0500

    toDos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 let inp = document.querySelector('.toDo__inp')
 let inp2 = document.querySelector('.toDo__inp2')
-let home = document.querySelector('.header__link')
+let home = document.querySelector('.clock__tit')
 let soooot = document.querySelector('.clock__card')
-
 let clock = document.querySelector('.header__linkk')
-
+let sot_head = document.querySelector('.clock')
+let sot = document.querySelector('.clock__card')
+let utsenkaCard = document.querySelector('.utsenka___wrap')
+let utsenkaH1 = document.querySelector('.utsenka__tit')
+let utsenkaParem = document.querySelector('.addUtsenk')
+console.log(utsenkaParem);
 let btn = document.querySelector('#toDo__btn')
 
 let form = document.querySelector('.toDo__form')
@@ -44,7 +48,7 @@ function addTodo(e){
   let newTime = new Todoss(inp.value,inp2.value,new Date().getHours(),new Date().getMinutes(),new Date().getSeconds(),)
   console.log(newTime);
     toDos.push(newTime)
-    console.log(toDos);
+  //  console.log(toDos);
     localStorage.setItem('toDos',JSON.stringify(toDos))
     render();
     console.log(inp.value);
@@ -60,13 +64,14 @@ const render = function(){
   //const TodoRes =   JSON.parse(localStorage.getItem("toDos"))
   toDos?.forEach((item,index)=>{
     res.innerHTML += `
-    <div class="res__div" class="${toDos.isCompled ? "":""}">
+    <div class="res__div"  class="${toDos.isCompled ? "":""}">
    
     <img class="res__img" src="https://img.razrisyika.ru/kart/34/1200/132496-poleznaya-eda-2.jpg" alt="">
     <h1  class="res__tit">${index +1}</h1>
 
     <h2  class="res__tit">${item.name}</h2>
-    <h2  class="res__tit">${item.narxi}ming</h2>
+    <h2  class="res__tit">${item.narxi},000</h2>
+    <span data-item-i="${index}" class='health'>❤️</span>
 
    
     </div>
@@ -74,6 +79,10 @@ const render = function(){
     `
   })
 }
+let utsenka = document.querySelector('.aksiya')
+let utsenkaBtn = document.querySelector('.aksia__btn')
+
+
 //console.log(e.target.getA);
 const sortTodo =()=>{
 console.log('changing');
@@ -173,6 +182,19 @@ function setAsos(){
 setRotation(milSecond,seconds)
 setRotation(milMinut,minuts)
 setRotation(milSoat,hours)
+let future = new Date("30 May 2024").getTime()
+
+setInterval (()=>{
+  let nov = new Date().getTime()
+  const Differance = future - nov
+  let day = Math.floor(Differance / (1000*60*60*24))
+  let soat = Math.floor(Differance / (1000*60*60))
+  let minut = Math.floor(Differance / (1000*60)%60)
+  let second = Math.floor((Differance % (1000*60)/1000))
+//console.log(second);
+utsenka.innerText = `${day.toString().padStart(2,"0")}:${soat.toString().padStart(2,"0")}:${minut.toString().padStart(2,"0")}:${soat.toString().padStart(2,"0")}:${second.toString().padStart(2,"0")}`
+},1000)
+
 function setRotation(element,rotationRatio){
 {
   element.style.setProperty('--rotation',rotationRatio*360)
@@ -180,13 +202,79 @@ function setRotation(element,rotationRatio){
 }
 
 }
-console.log(setAsos());
 form.addEventListener('submit',addTodo);
 inp.addEventListener('input',filterTodo);
 inp2.addEventListener('input',filterNarxTodo);
+utsenkaBtn.addEventListener("click",(e)=>{
+  e.preventDefault()
+ console.log('ok');
+ utsenkaCard.classList.remove('hiden')
+ utsenkaH1.classList.remove('hiden')
+ const aksiyaItem = [
+  {
+    id:'1',
+    img:'https://static.insales-cdn.com/images/articles/1/4526/3912110/3aa1f62988e5381923469ac134e37568.jpg',
+    name:'osh',
+    narxi:"30000"
+  },
+  {
+    id:'2',
+    img:'https://img.razrisyika.ru/kart/142/1200/564275-shashlyk-1.jpg',
+    name:'shashlik',
+    narxi:"15000"
+  },
+  {
+    id:'3',
+    img:'https://i.artfile.ru/2200x1450_1054692_[www.ArtFile.ru].jpg',
+    name:'manti',
+    narxi:"5000"
+  },
+  {
+    id:'4',
+    img:'https://avatars.dzeninfra.ru/get-zen_doc/1781567/pub_5e6a3ad1b4e3014f4849a858_5e6a75a71fed487b1f85564a/scale_1200',
+    name:'honim',
+    narxi:"6000"
+  },
+]
+aksiyaItem.forEach((item,index)=>{
+  const $div = document.createElement('p')
+ $div.innerHTML = `
+ <div  class="res__divc">
+   
+    <img class="res__img" src="https://img.razrisyika.ru/kart/34/1200/132496-poleznaya-eda-2.jpg" alt="">
 
+    <h2  class="res__tit">${item.name}</h2>
+    <h2  class="res__tit">${item.narxi}</h2>
+    <span data-item-i="${index}" class="health2">❤️</span>
+    </div>
+ `
+ 
+ utsenkaParem.appendChild($div)
 
-resParent.addEventListener('dblclick',itemDelete);
+})
+})
+function heall(e){
+  console.log(e.target);
+  if(e.target.className === 'health'){
+  // const like= localStorage.setItem('toDos',JSON.stringify(toDos))
+   console.log(e.target.getAttribute("data-item-i"));
+    
 
+  }
+  }
+res.addEventListener('click',heall);
+clock.addEventListener("click",(e)=>{
+  e.preventDefault()
+ 
+  sot_head.classList.remove('hiden')
+  sot.classList.remove('hiden')
+})
+home.addEventListener("click",(e)=>{
+  e.preventDefault()
+  sot_head.classList.add('hiden')
+  sot.classList.add('hiden')
+ 
+  
+})
 sortSelect.addEventListener('change',sortTodo);
 
